56550a01071e93207d0e6be03d6d5d64fe8c41dc
J3rCast <jerson.castro.r@gmail.com>
2022-05-03 12:08:34 -0500

    last digit buf fix

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 lastDigit = number % 10
 
-if number > 0:
+if number >= 0:
     print(f"Last digit of {number} is {lastDigit}", end=" ")
 
     if lastDigit > 5:
