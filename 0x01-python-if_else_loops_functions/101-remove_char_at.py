7906bcb2e55475706dfa5c085651318923a5a798
J3rCast <jerson.castro.r@gmail.com>
2022-05-03 16:16:19 -0500

    remove char at added

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
 
-    if len(str) > n and n > 0:
+    if len(str) > n and n >= 0:
         strCopy = str.replace(str[n], '')
         return strCopy
     else:
