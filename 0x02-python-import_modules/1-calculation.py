355eaca3f2fd623ce2319df38249234166fb9d5c
J3rCast <jerson.castro.r@gmail.com>
2022-05-05 12:23:07 -0500

    bug fixes

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,7 +4,7 @@ a = 10
 b = 5
 
 if __name__ == "__main__":
-    print(f"{a} + {b} = {calculator.add(a, b)}")
-    print(f"{a} - {b} = {calculator.sub(a, b)}")
-    print(f"{a} * {b} = {calculator.mul(a, b)}")
-    print(f"{a} / {b} = {calculator.div(a, b)}")
+    print(f"10 + 5 = {calculator.add(a, b)}")
+    print(f"10 - 5 = {calculator.sub(a, b)}")
+    print(f"10 * 5 = {calculator.mul(a, b)}")
+    print(f"10 / 5 = {calculator.div(a, b)}")
