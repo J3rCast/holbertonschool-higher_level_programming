f4d8df71ce2c434f6145a4ea2f4178dbff992f5c
J3rCast <jerson.castro.r@gmail.com>
2022-05-05 12:11:34 -0500

    bug fixes

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,9 +3,10 @@ import calculator_1 as calculator
 a = 10
 b = 5
 
-dict = {'add': '+', 'sub': '-', 'mul': '*', 'div': '/'}
+if __name__ = "__main__"
+    dict = {'add': '+', 'sub': '-', 'mul': '*', 'div': '/'}
 
-print(f"{a} {dict['add']} {b} = {calculator.add(a, b)}")
-print(f"{a} {dict['sub']} {b} = {calculator.add(a, b)}")
-print(f"{a} {dict['mul']} {b} = {calculator.add(a, b)}")
-print(f"{a} {dict['div']} {b} = {calculator.add(a, b)}")
+    print(f"{a} {dict['add']} {b} = {calculator.add(a, b)}")
+    print(f"{a} {dict['sub']} {b} = {calculator.add(a, b)}")
+    print(f"{a} {dict['mul']} {b} = {calculator.add(a, b)}")
+    print(f"{a} {dict['div']} {b} = {calculator.add(a, b)}")
