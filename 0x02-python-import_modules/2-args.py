ad4095de97fb1c7720d9145dc0f09678e7ad69af
J3rCast <jerson.castro.r@gmail.com>
2022-05-05 12:35:26 -0500

    bug fixes

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,9 @@ if __name__ == "__main__":
     if len(argv) == 1:
         print(f"{len(argv) -1} arguments.")
     else:
-        print(f"{len(argv) -1} argument:")
+        if len(argv) == 2:
+            print(f"{len(argv) -1} argument:")
+        else:
+            print(f"{len(argv) -1} arguments:")
         for i in range(1, len(argv)):
             print(f"{i}: {argv[i]}")
