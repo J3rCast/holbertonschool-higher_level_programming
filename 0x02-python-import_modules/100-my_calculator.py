4999c4ffb70ae9b4c691981301d7023e49ff5861
J3rCast <jerson.castro.r@gmail.com>
2022-05-05 13:25:00 -0500

    my calculator added

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    # IMPORTS
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+
+    # VARIABLES
+    a = argv[1]
+    b = argv[3]
+
+    # CODE
+    if len(argv) != 4:
+        print(f"Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    if argv[2] != '+' and argv[2] != '-' and argv[2] != '*' and argv[2] != '/':
+        print(f"Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    if argv[2] == '+':
+        print(f"{a} + {b} = {add(int(a), int(b))}")
+    if argv[2] == '-':
+        print(f"{a} - {b} = {sub(int(a), int(b))}")
+    if argv[2] == '*':
+        print(f"{a} * {b} = {mul(int(a), int(b))}")
+    if argv[2] == '/':
+        print(f"{a} / {b} = {div(int(a), int(b))}")
