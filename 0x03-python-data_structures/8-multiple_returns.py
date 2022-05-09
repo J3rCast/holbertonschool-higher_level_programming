734db579f814ff624e85a9b11763a955a929e61a
J3rCast <jerson.castro.r@gmail.com>
2022-05-09 13:52:23 -0500

    multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,6 +3,6 @@ def multiple_returns(sentence):
     if not sentence:
         new_tuple = (0, None)
     else:
-        new_tuple = (len(sentece), sentence[0])
+        new_tuple = (len(sentence), sentence[0])
 
     return new_tuple
