c22bcacaa85ab465146fe0ce4de92cdbc5ecad76
J3rCast <jerson.castro.r@gmail.com>
2022-05-09 12:25:36 -0500

    print matrix integer bug fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix == [[]]:
-        print("$")
+        print()
     else:
         for i in range(len(matrix)):
             for idx in range(len(matrix[i])-1):
                 print(f"{matrix[i][idx]} ", end='')
                 if matrix[i][idx]:
-                    print(f"{matrix[i][idx+1]}$")
+                    print(f"{matrix[i][idx+1]}")
