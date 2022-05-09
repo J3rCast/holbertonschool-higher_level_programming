decbb934538eff24195dfa6a140497ceaa7eea2f
J3rCast <jerson.castro.r@gmail.com>
2022-05-09 14:00:19 -0500

    max integer added

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if not my_list:
         return None
 
-    biggest_number = 0
+    biggest_number = -100000
 
     for i in my_list:
         if i > biggest_number:
