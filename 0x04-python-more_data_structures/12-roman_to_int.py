4e66abc80dcaa4f2e923eadce9dc4656139becbc
J3rCast <jerson.castro.r@gmail.com>
2022-05-10 14:12:02 -0500

    roman to int added

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    aux = 0
+    roman_dict =\
+        {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    for i in range(len(roman_string)):
+        for j in roman_dict:
+            if i > 0:
+                if roman_string[i] == j \
+                        and roman_dict[roman_string[i - 1]] >= roman_dict[j]:
+                    aux += roman_dict[j]
+                elif roman_string[i] == j \
+                        and roman_dict[roman_string[i - 1]] < roman_dict[j]:
+                    aux -= roman_dict[roman_string[i - 1]]
+                    aux -= roman_dict[roman_string[i - 1]]
+                    aux += roman_dict[j]
+            elif roman_string[i] == j:
+                aux += roman_dict[j]
+    return aux
