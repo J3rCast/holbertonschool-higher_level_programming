8c3ef32e071b8124eacf7e78ed15aeca69d15b15
J3rCast <jerson.castro.r@gmail.com>
2022-05-23 12:32:49 -0500

    pycodestyle fixes

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for i in range(x):
-            print(my_list[i], end='')
-        print()
-        return i + 1
-    except (IndexError):
-        print()
-        pass
-
-    return i
+    index = 0
+    for index in range(x):
+        try:
+            print(my_list[index], end='')
+        except IndexError:
+            print()
+            return index
+    print()
+    return index + 1
