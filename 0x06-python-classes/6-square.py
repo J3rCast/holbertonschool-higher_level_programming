3bb3bb100645ffde432b3f2c2884d627896abe8e
J3rCast <jerson.castro.r@gmail.com>
2022-05-24 17:28:15 -0500

    6 task added

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -14,9 +14,6 @@ class Square:
             print("size must be >= 0", end="")
             raise ValueError
         self.__position = position
-        if self.__position[0] < 0 or self.__position[1] < 0 or len(self.__position) != 2:
-            print("position must be a tuple of 2 positive integers")
-            raise TypeError
 
     def area(self):
         """return the area of the square"""
@@ -46,8 +43,7 @@ class Square:
     @position.setter
     def position(self, value):
         """setter of the position"""
-        if value[0] < 0 or value[1] < 0\
-                or type(value[0]) != int or type(value[1]) != int:
+        if value[0] < 0 or value[1] < 0:
             print("position must be a tuple of 2 positive integers", end="")
             raise TypeError
         self.__position = value
