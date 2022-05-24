4272fb11d9be8a08e4a79018a9d5afffa4c0b187
J3rCast <jerson.castro.r@gmail.com>
2022-05-24 18:42:00 -0500

    102 added

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Create a class called square."""
+
+
+class Square:
+    """class Square that defines a square by: (based on 3-square.py)"""
+    def __init__(self, size=0):
+        """Private instance attribute: size"""
+        self.__size = size
+        if type(self.__size) != int:
+            print("size must be an integer", end="")
+            raise TypeError
+        elif self.__size < 0:
+            print("size must be >= 0", end="")
+            raise ValueError
+
+    def __new__(cls, num):
+        """return the value of the class"""
+        return num
+
+    def area(self):
+        """return the area of the square"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """getter of the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, new_size):
+        """setter of the size"""
+        if type(new_size) != int:
+            print("size must be an integer", end="")
+            raise TypeError
+        elif new_size < 0:
+            print("size must be >= 0", end="")
+            raise ValueError
+        self.__size = new_size
