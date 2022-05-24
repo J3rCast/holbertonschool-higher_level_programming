c732f461f657f5b59201e9df6a07993a06849a2b
J3rCast <jerson.castro.r@gmail.com>
2022-05-24 15:53:07 -0500

    bug fixes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 class Square:
-    '''Write an empty class Square that defines a square'''
+    """Write an empty class Square that defines a square
+
+    sdaf j asdjfas dj fasdjfa djafd jfad j
+    """
     pass
