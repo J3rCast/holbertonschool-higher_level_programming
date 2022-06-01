922deed2998967bda20fa4b529b4e56f35ff8341
J3rCast <jerson.castro.r@gmail.com>
2022-06-01 18:20:34 -0500

    pycodestyle fixes

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""This module contain a class that 
+"""This module contain a class that
 prevents the user from dynamically creating
-new instance attributes, except if the 
+new instance attributes, except if the
 new instance attribute is called first_name.
 """
+
 
 class LockedClass:
     """This class creates instance
     only if is called first_name
     """
     __slots__ = ['first_name']
-    
+
     def __init__(self, first_name=""):
         """This method initialize the object."""
         if hasattr(self, first_name):
