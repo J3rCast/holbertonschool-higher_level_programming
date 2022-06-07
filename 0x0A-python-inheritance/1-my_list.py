ff6c87e841a97cf42688b48b2785c8218f94a8f4
J3rCast <jerson.castro.r@gmail.com>
2022-06-07 18:48:23 -0500

    documentation fixes

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -9,9 +9,5 @@ class MyList(list):
 
     def print_sorted(self):
         """prints in ascending order"""
-        new_list = []
-        for i in self:
-            new_list.append(i)
-
-        new_list.sort()
-        print(new_list)
+        
+        print(sorted(self))
