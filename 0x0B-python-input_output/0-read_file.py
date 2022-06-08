388264eba2c6f633ec9692ffd8769fd3f5d446e5
J3rCast <jerson.castro.r@gmail.com>
2022-06-08 16:56:20 -0500

    some task added

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,6 +7,4 @@ reads a text file
 def read_file(filename=""):
     """This function reads a file"""
     with open(filename, 'r', encoding="utf-8") as file:
-        fileReaded = file.read()
-
-    print(fileReaded)
+        print(file.read(), end="")
