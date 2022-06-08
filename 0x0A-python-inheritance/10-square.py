32a3a3ef3f4ebfc3f20433e17ec9fe212a738061
J3rCast <jerson.castro.r@gmail.com>
2022-06-07 20:12:11 -0500

    documentation fixes

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -3,41 +3,7 @@
 """
 
 
-class BaseGeometry:
-    """This class is the parent class of geometry"""
-
-    def area(self):
-        """this method defines the area"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """This method validates value"""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """This method is child class that defines a
-    Rectangel
-    """
-    def __init__(self, width, height):
-        """This method initialize the class"""
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
-        self._Rectangle__width = width
-        self._Rectangle__height = height
-
-    def area(self):
-        """This method return the area"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """This method is called when "print" or "str"
-        is called
-        """
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
