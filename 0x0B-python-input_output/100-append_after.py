97ecdf12fb7c0ff641c3be722f6acdfd051c3edb
J3rCast <jerson.castro.r@gmail.com>
2022-06-08 19:15:08 -0500

    100 task added

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -12,12 +12,12 @@ def append_after(filename="", search_string="", new_string=""):
 
     with open(filename, 'r', encoding="utf-8") as f:
         lines = f.readlines()
-    
+
     for line in lines:
         idx += 1
         if search_string in line:
             lines.insert(idx, new_string)
-    
+
     with open(filename, 'w', encoding="utf-8") as f:
         lines = "".join(lines)
         f.write(lines)
