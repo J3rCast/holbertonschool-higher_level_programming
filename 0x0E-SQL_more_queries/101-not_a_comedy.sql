1c91c8de799cebd66e2d3be4d9efa9d4bf3ee84e
J3rCast <jerson.castro.r@gmail.com>
2022-07-15 09:50:37 -0500

    task 18 added

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -12,8 +12,8 @@ LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id
-WHERE name NOT IN (
-	SELECT name
+WHERE tv_shows.id NOT IN (
+	SELECT tv_shows.id
 	FROM tv_shows
 	LEFT JOIN tv_show_genres
 	ON tv_shows.id = tv_show_genres.show_id
