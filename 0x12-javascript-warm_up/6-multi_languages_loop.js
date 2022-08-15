7e3d79851b30423e6054aa326cc801d24091109a
J3rCast <jerson.castro.r@gmail.com>
2022-08-15 15:10:05 -0500

    7 task added

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 let i = 0;
-const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 while (i < 3) {
-  console.log(array[i]);
+  console.log(strings[i]);
   i++;
 }
