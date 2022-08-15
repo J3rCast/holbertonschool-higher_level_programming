1f4a8f4dfde75b6d8b9b8efa8a543abd50977565
J3rCast <jerson.castro.r@gmail.com>
2022-08-15 15:52:09 -0500

    seventh task added

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-import { argv } from 'node:process'
+import { argv } from 'node:process';
 
-if (!argv[2]) console.log('No argument')
-else console.log(argv[2])
+if (!argv[2]) console.log('No argument');
+else console.log(argv[2]);
