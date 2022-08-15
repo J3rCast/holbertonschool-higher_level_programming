4f610afd0f4400face33e767b406c58060d3c561
J3rCast <jerson.castro.r@gmail.com>
2022-08-15 18:08:54 -0500

    12 task added

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,7 @@ const args = process.argv;
 let i = 2;
 let res = 0;
 let max = 0;
+let temp = -Infinity;
 const list = [];
 const len = args.length;
 
@@ -16,7 +17,7 @@ max = Math.max(...list);
 if (!args[2] || !args[3]) console.log(0);
 else {
   while (i < len) {
-    if (args[i] < max && args[i] > args[i - 1]) res = args[i];
+    if (args[i] < max && args[i] > temp) temp = args[i], res = args[i];
     i++;
   }
   console.log(parseInt(res));
