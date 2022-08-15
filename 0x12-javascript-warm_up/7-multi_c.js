5198cedcc26fe4d951c47dc9f4bcb70af653d1c5
J3rCast <jerson.castro.r@gmail.com>
2022-08-15 16:17:10 -0500

    eight task added

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,6 @@ import { argv } from 'node:process';
 
 if (isNaN(argv[2])) {
   console.log('Missing number of occurrences');
-}
-else {
+} else {
   for (let i = 0; i < parseInt(argv[2]); i++) console.log('C is fun');
 }
