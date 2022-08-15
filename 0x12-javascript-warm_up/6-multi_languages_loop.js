a40e75980b0460f84b43e69707bfd89143dc3482
J3rCast <jerson.castro.r@gmail.com>
2022-08-15 16:01:22 -0500

    seventh task added

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-let i = 0;
-const str = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (i = 0; i < str.length; i++) {
-  console.log(str[i]);
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i;
+for (i = 0; languages[i]; i++) {
+  console.log(languages[i]);
 }
