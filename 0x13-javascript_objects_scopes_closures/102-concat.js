1fb184c854ae1d3670058914b43cd16323b51f4e
J3rCast <jerson.castro.r@gmail.com>
2022-08-16 16:55:14 -0500

    last task added

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,11 +2,18 @@
 const fs = require('fs');
 const args = process.argv;
 
-fs.readFile(args[2], 'utf8', function(err, data1) {
-  fs.readFile(args[3], 'utf8', function(err, data2) {
+fs.readFile(args[2], 'utf8', function (err, data1) {
+  if (err) {
+    throw err;
+  }
+  fs.readFile(args[3], 'utf8', function (err, data2) {
+    if (err) {
+      throw err;
+    }
     fs.appendFile(args[4], data1 + data2, (err) => {
-      return;
+      if (err) {
+        throw err;
+      }
     });
   });
 });
-
