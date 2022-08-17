6755514ba4ad8e623c3c69407995b13312156dc5
J3rCast <jerson.castro.r@gmail.com>
2022-08-17 12:38:52 -0500

    first task fixed

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-const argv = require('process');
+const { argv } = require('process');
 
 fs.readFile(argv[2], 'utf-8', (data, err) => {
   if (err) console.log(err);
