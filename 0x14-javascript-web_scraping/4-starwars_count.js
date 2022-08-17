72e442e3b1e1a44dc1ce43199f85955d22721446
J3rCast <jerson.castro.r@gmail.com>
2022-08-17 15:32:44 -0500

    task five added

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const axios = require('axios');
+const { argv } = require('process');
+const link = argv[2];
+const character = 'https://swapi-api.hbtn.io/api/people/18/';
+
+axios.get(link)
+  .then(res => {
+    let movieCount = 0;
+    for (let i = 0; i < res.data.count; i++) {
+      if (Object.values(res.data.results[i].characters).includes(character)) {
+        movieCount++;
+      }
+    }
+    console.log(movieCount);
+  }).catch(error => {
+    if (error.response) {
+      console.log(error.response.status);
+    }
+  });
