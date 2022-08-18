8d28ff065fc406d188f38544d277691e86b9a009
J3rCast <jerson.castro.r@gmail.com>
2022-08-18 07:55:36 -0500

    task four fixed

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,7 +7,7 @@ const character = 'https://swapi-api.hbtn.io/api/people/18/';
 axios.get(link)
   .then(res => {
     let movieCount = 0;
-    for (let i = 0; i < res.data.count; i++) {
+    for (let i = 0; i < res.data.results.length; i++) {
       if (Object.values(res.data.results[i].characters).includes(character)) {
         movieCount++;
       }
