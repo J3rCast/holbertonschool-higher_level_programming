9ddfc1f74521c85feb510a5d8cbaa516b892a8a1
J3rCast <jerson.castro.r@gmail.com>
2022-08-30 13:07:52 -0500

    third task added

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-    Script that takes in arguments and displays all values in the states table of
-    hbtn_0e_0_usa where name matches the argument. But this time, write one that
-    is safe from MySQL injections!
+    Script that takes in arguments and displays all values in the
+    states table of hbtn_0e_0_usa where name matches the argument.
+    But this time, write one that is safe from MySQL injections!
 """
 
 
@@ -13,7 +13,7 @@ import MySQLdb
 def main():
     db = MySQLdb.connect('localhost', argv[1], argv[2], argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name = %s", (argv[4],))
+    cur.execute("SELECT * FROM states WHERE BINARY name = %s;", (argv[4],))
     rows = cur.fetchall()
     for row in rows:
         print(row)
