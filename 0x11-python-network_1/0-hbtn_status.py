65ea9ee11649397ed036b2f4e2c2e48d7d23b64e
J3rCast <jerson.castro.r@gmail.com>
2022-09-02 11:10:39 -0500

    all task added

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""script that fetches https://intranet.hbtn.io/status"""
+
+
+import urllib.request
+
+
+if __name__ == "__main__":
+    req = urllib.request.Request('https://intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(the_page)}")
+        print(f"\t- content: {the_page}")
+        print(f"\t- utf8 content: {the_page.decode('utf-8')}")
---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" takes in a URL, sends a request to the URL
+    and displays the body of the response (decoded in utf-8).
+"""
+
+
+import urllib.request
+from urllib.parse import urlencode
+from sys import argv
+
+
+if __name__ == "__main__":
+    email = {"email": argv[2]}
+    data = urlencode(email).encode("utf-8")
+    req = urllib.request.Request(argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        print(the_page.decode("utf-8"))
